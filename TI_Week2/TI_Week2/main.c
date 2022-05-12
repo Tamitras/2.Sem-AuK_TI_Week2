211ba7768068e23321ee847e343cf8d0f3614481
Erik <erik.kaufmann91@gmail.com>
2022-05-12 11:14:23 +0200

    Added project

---
+++ TI_Week2/TI_Week2/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+	// Enable to attach with debugger
+	//printf("Double_Num\n");
+	//Sleep(8000);
+
+
+
+
+
+	// Exit application with success-code
+	exit(EXIT_SUCCESS);
+}
